0a5185399242f3d244512c09e4ed940b7a18505d
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-06 13:42:45 +0000

    [Add]Edit function

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,18 @@ Rails.application.routes.draw do
   # get 'lists/index'
   # 「詳細画面を作ろう」のRoutingで削除する
   # get 'lists/show'
-  get 'lists/edit'
+  # 「編集機能を作ろう」のRoutingで削除する
+  # get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   # 「一覧画面表示」のRoutingで作成する
   get 'lists' => 'lists#index'
   # 「詳細画面を作ろう」のRouting追加
   get 'lists/:id' => 'lists#show', as:'list'
+  # 「編集機能を作ろう」のRouting追加
+  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  # 「編集機能を作ろう」でフォームとアクションを紐付ける。
+  # 更新のHTTPメソッドは「PATCH」
+  patch 'lists/:id' => 'lists#update',as:'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
