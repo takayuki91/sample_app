944d94ed0faf018f31cc6404b7eab9842c80f3c4
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-06 06:02:55 +0000

    [Add]List screen display

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
+  # 「一覧画面表示」のRoutingで削除する
+  # get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
+  # 「一覧画面表示」のRoutingで作成する
+  get 'lists' => 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
