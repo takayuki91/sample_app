6766c2a6d0ee596b45d113106bd2ac84b71090f9
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-07 10:28:09 +0000

    [Add]Delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,30 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  # 「一覧画面表示」のRoutingで削除する
-  # get 'lists/index'
-  # 「詳細画面を作ろう」のRoutingで削除する
-  # get 'lists/show'
-  # 「編集機能を作ろう」のRoutingで削除する
-  # get 'lists/edit'
+  # resourcesメソッドで記述を簡単にする
+  # 以下、全て削除
+  
+  # get 'lists/new'
+  # # 「一覧画面表示」のRoutingで削除する
+  # # get 'lists/index'
+  # # 「詳細画面を作ろう」のRoutingで削除する
+  # # get 'lists/show'
+  # # 「編集機能を作ろう」のRoutingで削除する
+  # # get 'lists/edit'
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  # 「一覧画面表示」のRoutingで作成する
-  get 'lists' => 'lists#index'
-  # 「詳細画面を作ろう」のRouting追加
-  get 'lists/:id' => 'lists#show', as:'list'
-  # 「編集機能を作ろう」のRouting追加
-  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
-  # 「編集機能を作ろう」でフォームとアクションを紐付ける。
-  # 更新のHTTPメソッドは「PATCH」
-  patch 'lists/:id' => 'lists#update',as:'update_list'
+  # post 'lists' => 'lists#create'
+  # # 「一覧画面表示」のRoutingで作成する
+  # get 'lists' => 'lists#index'
+  # # 「詳細画面を作ろう」のRouting追加
+  # get 'lists/:id' => 'lists#show', as:'list'
+  # # 「編集機能を作ろう」のRouting追加
+  # get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  # # 「編集機能を作ろう」でフォームとアクションを紐付ける。
+  # # 更新のHTTPメソッドは「PATCH」
+  # patch 'lists/:id' => 'lists#update',as:'update_list'
+  # # 「削除機能」でRouting追加。
+  # # 削除のHTTPメソッドは「delete」
+  # delete 'lists/:id' => 'lists#destroy',as:'destroy_list'
+  
+  # 以下、追加
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
